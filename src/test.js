966eba7e19a2c8812893fe837e95764833092dc8
JanLazz <jan.lazzari@posteo.de>
2023-11-01 14:22:50 +0100

    just for saving

--- src/test.js
+++ src/test.js
@@ -1,14 +1,15 @@
-
-import React from 'react';
+import React, { useState } from 'react';
+import { JSONTree } from 'react-json-tree';
+import { Map } from 'immutable';
 import * as michelsonImport from "michelson-interpreter";
 
-console.log("MichelsonImport:", michelsonImport);
 const michelsonInterpreter = michelsonImport.default.michelsonInterpreter;
 const State = michelsonImport.default.State;
-console.log("michelsonInterpreter: ", michelsonInterpreter);
-console.log("State: ", State);
 
 function MyMichelsonComponent() {
+  const [interpreterOutput, setInterpreterOutput] = useState(null);
+  const [currentStep, setCurrentStep] = useState(0);
+
   const handleFileChosen = async (event) => {
     const file = event.target.files[0];
     const fileReader = new FileReader();
@@ -16,15 +17,38 @@ function MyMichelsonComponent() {
       const script = e.target.result;
       const state = new State('', '', 0, 'default', 0, 0, 0);
       const michelsonReturnValue = michelsonInterpreter(script, 0.1, 0.1, state);
-      console.log(michelsonReturnValue);
-      console.log("File on load");
+      const parsedOutput = JSON.parse(michelsonReturnValue);
+      setInterpreterOutput(parsedOutput);
+      setCurrentStep(0);
     };
     fileReader.readAsText(file);
+  };
+
+  const handleNextStep = () => {
+    if (currentStep < interpreterOutput.length - 1) {
+      setCurrentStep(currentStep + 1);
+    }
+  };
+
+  const handlePrevStep = () => {
+    if (currentStep > 0) {
+      setCurrentStep(currentStep - 1);
+    }
   };
 
   return (
     <div>
       <input type="file" onChange={handleFileChosen} />
+      {interpreterOutput && (
+        <div>
+          <button onClick={handlePrevStep} disabled={currentStep === 0}>Back</button>
+          <button onClick={handleNextStep} disabled={currentStep === interpreterOutput.length - 1}>Next</button>
+          <div>
+            <h>Step {currentStep + 1}</h>
+            <pre>{JSON.stringify(interpreterOutput[currentStep], null, 2)}</pre>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
