7e3fd370470f0aeb5217d91b1dee17f21f00d9cb
JanLazz <jan.lazzari@posteo.de>
2023-11-15 09:53:46 +0100

    stable & pretty version

--- src/test.js
+++ src/test.js
@@ -1,35 +1,58 @@
-import React, { useState } from 'react';
-import { JSONTree } from 'react-json-tree';
-import { Map } from 'immutable';
+import React, { useState } from "react";
+import { JSONTree } from "react-json-tree";
+import { Map } from "immutable";
 import * as michelsonImport from "michelson-interpreter";
-import Rjv from 'react-json-tree-viewer';
+import Rjv from "react-json-tree-viewer";
+import "./MyMichelsonComponent.css";
 
 const michelsonInterpreter = michelsonImport.default.michelsonInterpreter;
 const State = michelsonImport.default.State;
 
 function MyMichelsonComponent() {
+  const [fileIsIn, setFileIsIn] = useState(null);
+  const [file, setFile] = useState(null);
+  const [script, setScript] = useState(null);
   const [interpreterOutput, setInterpreterOutput] = useState(null);
   const [currentStep, setCurrentStep] = useState(0);
+  const [parameterInfo, setParameterInfo] = useState(null);
+  const [storageInfo, setStorageInfo] = useState(null);
+  const [parameterInput, setParameterInput] = useState("");
+  const [storageInput, setStorageInput] = useState("");
   const jsonData = {
-    key: 'value',
+    key: "value",
     nested: {
-      nestedKey: 'nestedValue',
+      nestedKey: "nestedValue",
     },
   };
 
-
-  const handleFileChosen = async (event) => {
+  const prepareFileChosen = async (event) => {
     const file = event.target.files[0];
     const fileReader = new FileReader();
     fileReader.onload = async (e) => {
       const script = e.target.result;
-      const state = new State('', '', 0, 'default', 0, 0, 0);
-      const michelsonReturnValue = michelsonInterpreter(script, 0.1, 0.1, state);
-      const parsedOutput = JSON.parse(michelsonReturnValue);
-      setInterpreterOutput(parsedOutput);
-      setCurrentStep(0);
+      const splitScript = script.split(";");
+      const parameter = splitScript[0].split("parameter ")[1].trim();
+      const storage = splitScript[1].split("storage ")[1].trim();
+      setScript(script);
+      setParameterInfo(parameter);
+      setStorageInfo(storage);
+      setFileIsIn(true);
     };
-    fileReader.readAsText(file);
+    setFile(fileReader.readAsText(file));
+  };
+
+  const handleFileWithParameters = async (event) => {
+    event.preventDefault();
+    const state = new State("", "", 0, "default", 0, 0, 0);
+    const michelsonReturnValue = michelsonInterpreter(
+      script,
+      parameterInput,
+      storageInput,
+      state
+    );
+    const parsedOutput = JSON.parse(michelsonReturnValue);
+    setInterpreterOutput(parsedOutput);
+    setCurrentStep(0);
   };
 
   const handleNextStep = () => {
@@ -44,86 +67,107 @@ function MyMichelsonComponent() {
     }
   };
 
-  const theme = {
-    scheme: 'solarized',
-    author: 'ethan schoonover (http://ethanschoonover.com/solarized)',
-    base00: '#F5FFF0',
-    base01: '#073642',
-    base02: '#586e75',
-    base03: '#657b83',
-    base04: '#839496',
-    base05: '#93a1a1',
-    base06: '#eee8d5',
-    base07: '#fdf6e3',
-    base08: '#dc322f',
-    base09: '#cb4b16',
-    base0A: '#b58900',
-    base0B: '#859900',
-    base0C: '#2aa198',
-    base0D: '#268bd2',
-    base0E: '#6c71c4',
-    base0F: '#d33682'
+  const handleParameterInputChange = (event) => {
+    setParameterInput(event.target.value);
+  };
+
+  const handleStorageInputChange = (event) => {
+    setStorageInput(event.target.value);
   };
 
   return (
-    <div className="my-michelson-component">
-      <input type="file" onChange={handleFileChosen} style={{margin: '20px'}}/>
-      {interpreterOutput && (
-        <div>
-          <button className="nav-button" onClick={handlePrevStep} disabled={currentStep === 0} style={{margin: '10px'}}>Back</button>
-          <button className="nav-button" onClick={handleNextStep} disabled={currentStep === interpreterOutput.length - 1} style={{margin: '10px'}}>Next</button>
+    <body>
+      <h3>Michelson Interpreter</h3>
+      <div className="my-michelson-component">
+        <input
+          type="file"
+          onChange={prepareFileChosen}
+          style={{ margin: "20px" }}
+        />
+        {fileIsIn && (
+          <form>
+            <div className="input-info" style={{ marginBottom: "25px" }}>
+              <h4 className="input-label">
+                Parameter (type: {parameterInfo})
+                <br />
+                Storage (type: {storageInfo})
+              </h4>
+            </div>
+            <div>
+              <label style={{ marginBottom: "10px" }}>
+                <span style={{ marginRight: "10px" }}>Parameter: </span>
+                <input
+                  type="text"
+                  value={parameterInput}
+                  onChange={handleParameterInputChange}
+                  placeholder="Bitte Parameter eingeben"
+                />
+              </label>
+              <label style={{ marginBottom: "10px" }}>
+                <span style={{ marginRight: "10px", marginLeft: "40px" }}>
+                  Storage:{" "}
+                </span>
+                <input
+                  type="text"
+                  value={storageInput}
+                  onChange={handleStorageInputChange}
+                  placeholder="Bitte Storage eingeben"
+                />
+              </label>{" "}
+              <button
+                className="submit-input-button"
+                onClick={handleFileWithParameters}
+                disabled={parameterInput === "" || storageInput === ""}
+                style={{ margin: "10px" }}
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        )}
+        {interpreterOutput && (
           <div>
-            <div className="parameter-json-container">
-              <div className="parameter-info">
-                <h className="parameter-label" style={{margin: '20px'}}>Parameter</h>
-                <pre >{`parameter int;`}</pre>
-              </div >
-              <h className="step-label" style={{margin: '20px'}}>Step {currentStep + 1}</h>
-              <div style={{ display: 'flex', justifyContent: 'center', margin: '20px'}}>
-              <Rjv 
-                data={interpreterOutput[currentStep]}
-                hideRoot={true} 
-                style={{ margin: '10px' }}
-                arrowStyle={{ color: 'black' }}
-                shouldExpandNode={(path, data) => path.length === 1}
-              />
+            <button
+              className="nav-button"
+              onClick={handlePrevStep}
+              disabled={currentStep === 0}
+              style={{ margin: "10px" }}
+            >
+              Back
+            </button>
+            <button
+              className="nav-button"
+              onClick={handleNextStep}
+              disabled={currentStep === interpreterOutput.length - 1}
+              style={{ margin: "10px" }}
+            >
+              Next
+            </button>
+            <div>
+              <h className="step-label" style={{ margin: "20px" }}>
+                Step {currentStep + 1}
+              </h>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  margin: "20px",
+                }}
+              >
+                <Rjv
+                  data={interpreterOutput[currentStep]}
+                  hideRoot={true}
+                  style={{ margin: "10px" }}
+                  arrowStyle={{ color: "black" }}
+                  shouldExpandNode={(path, data) => path.length === 1}
+                />
               </div>
             </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </body>
   );
 }
-
-/*
- <div style={{ display: 'flex', justifyContent: 'center', margin: '20px'}}>
-              <Rjv 
-                data={interpreterOutput[currentStep]}
-                hideRoot={true} 
-                style={{ margin: '10px' }}
-                arrowStyle={{ color: 'black' }}
-                shouldExpandNode={(path, data) => path.length === 1}/>
-            </div>
-*/
-
-/*<div>
-              <JSONTree
-                data={interpreterOutput[currentStep]}
-                hideRoot={true}
-                theme={{
-                  extend: theme,
-                  valueLabel: {
-                    textDecoration: 'underline',
-                  },
-                  nestedNodeLabel: ({ style }, keyPath, nodeType, expanded) => ({
-                    style: {
-                      ...style,
-                      textTransform: expanded ? 'uppercase' : style.textTransform,
-                    },
-                  }),
-                }}
-              /> 
-            </div>*/
 
 export default MyMichelsonComponent;
